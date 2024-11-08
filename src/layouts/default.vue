dc532bfc9ff57408c0b6de73f828d07561c9668d
Xu Yuandong <786018072@qq.com>
2024-11-08 06:39:05 +0000

    U

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,11 @@
 <template>
   <v-app>
+  <v-app-bar></v-app-bar>
   <v-navigation-drawer></v-navigation-drawer>
-  <v-app-bar tile="app-bar12"></v-app-bar>
   <v-main>
     <router-view />
   </v-main>
+  <AppFooter />
   </v-app>
 </template>
 
